4d9377712417a73f312056bc0e817c281acfdce0
ExC0d3 <sharma.abhinav503@gmail.com>
2015-10-18 14:20:03 +0530

    Script functioning

--- script.js
+++ script.js
@@ -4,29 +4,7 @@ var user = $('#username');
 var text = $('#message');
 var send_button = $('#post');
 
-send_button.click(function() {
-	/* Act on the event */
-	myRef.push({
-		username: user.val(),
-		message: text.val()
-	});
 
-	text.val('');
-});
-
-myRef.on('child_added', function(snapshot) {
-    msg = snapshot.val();
-    var new_message = $('<div/>');
-    new_message.append('<p><strong>' + msg.username + '</strong></p><p>' + msg.message + '</p>');
-    //add a class for styling purposes    
-    new_message.addClass('msg');
-
-    // to differentiate between your messages and someone else's messages, we'll add a class 'me'
-    // to your messages and style them accordingly
-    new_message.addClass(msg.username == user.val() ? ' me' : '')   // this is an inline version of if-else    
-
-    $("#results").append(new_message);
-});
 
 var google_login_button = $('#google_login');
 
@@ -36,13 +14,40 @@ google_login_button.click(function() {
 	    if(error){
 	      console.log('login failed');
 	    }else{
-	      username =  authData.google.username;
+	      google_username =  authData.google.displayName;
 	      //add the username to the post button
-	      send_button.html("Post as " + user);
-
+	      send_button.html("Post as " + google_username);
+	      console.log("Successfully autheticated: " + google_username);
 	      // enable the post message button
-	      send_button.attr('disabled', 'false');
-	      google_login_button.attr('disabled', 'true');
+
+
+	      
+
+	      send_button.click(function() {
+	      	/* Act on the event */
+	      	myRef.push({
+	      		username: google_username,
+	      		message: text.val()
+	      	});
+
+	      	text.val('');
+	      });
+
+
+
+	      myRef.on('child_added', function(snapshot) {
+	          msg = snapshot.val();
+	          var new_message = $('<div/>');
+	          new_message.append('<p><strong>' + msg.username + '</strong></p><p>' + msg.message + '</p>');
+	          //add a class for styling purposes    
+	          new_message.addClass('msg');
+
+	          // to differentiate between your messages and someone else's messages, we'll add a class 'me'
+	          // to your messages and style them accordingly
+	          new_message.addClass(msg.username == user.val() ? ' me' : '')   // this is an inline version of if-else    
+
+	          $("#results").append(new_message);
+	      });
 	    }
 	  }, {remember: "none"}  // this will end authentication when the page is closed
 	  );
